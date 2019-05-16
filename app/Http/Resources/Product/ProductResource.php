160b5f0a5a5a06b9087964108e3da41018a62046
deyrupam <deyrupam161@gmail.com>
2019-05-16 16:48:55 +0530

    Product details modified

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'descriptions' =>$this->details,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock == 0 ? "Out of stock" : $this->stock,
+            'rating'=>$this->reviews->count()> 0 ? round($this->reviews->sum('star')/
+            $this->reviews->count(),2) : "NO rating yet",
+            'discount'=>$this->discount,
+            'total_price'=>round((1-($this->discount/100)) * $this->price,2),
+            'href'=>[
+               'reviews'=>route('reviews.index',$this->id)
+            ]
         ];
     }
 }
